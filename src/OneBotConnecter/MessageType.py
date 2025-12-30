224ba1b0170c177c9071a6a054e29d8846110328
Sugar51243 <1733682365@qq.com>
2025-12-30 08:45:08 +0800

    更新 MessageType.py

--- src/OneBotConnecter/MessageType.py
+++ src/OneBotConnecter/MessageType.py
@@ -16,7 +16,7 @@ class TextMessage(Message):
     text = ""
 
     def __init__(self, data: str):
-        if len(data) > 1000:
+        if len(data) > 3420:
             raise ValueError("内容过长")
         self.text = data
     
